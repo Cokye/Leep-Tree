db2c04cc325b56159fe951c29dd26b38e983ebd2
C0kyez <felipemauricio.mario@gmail.com>
2021-12-01 17:27:32 -0300

    ver14042020-1

---
+++ index.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("Principal.html")
+
+@app.route("/about")
+def about():
+    return render_template("Home.html")
+
+@app.route("/Tu_Arbol")
+def Tu_Arbol():
+    return render_template("Tu_Arbol.html")
+
+@app.route("/Desempeño")
+def Desempeño():
+    return render_template("Desempeño.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
